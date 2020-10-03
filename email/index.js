e3058b66ded03d440e51643548e0981f91a2c98b
Quan Dang <18520339@gm.uit.edu.vn>
2020-10-04 03:17:34 +0700

    Update index.js

--- email/index.js
+++ email/index.js
@@ -6,29 +6,27 @@ const fs = require('fs'); // built-in package
 const template = fs.readFileSync(`${__dirname}/template.hjs`, 'utf-8');
 const compiledTemplate = hogan.compile(template); // string => hogan
 
-module.exports.sendBookTicketEmail = email => {
-	const transport = {
-		host: 'smtp.gmail.com',
-		port: 587,
-		secure: false,
-		requireTLS: True,
-		requireSSL: True,
-		auth: {
-			user: '',
-			pass: '',
-		},
-	};
-
-	const transporter = nodemailer.createTransport(transport);
-	const mailOptions = {
-		from: '',
-		to: email,
-		subject: 'Ticket Booking Infomation',
-		html: compiledTemplate.render({ email }),
-	};
-
-	transporter.sendMail(mailOptions, err => {
-		if (err) return console.log(err);
-		console.log('Success');
-	});
+const transport = {
+	host: 'smtp.gmail.com',
+	port: 587,
+	secure: false,
+	requireTLS: true,
+	requireSSL: true,
+	auth: {
+		user: '',
+		pass: '',
+	},
 };
+
+const transporter = nodemailer.createTransport(transport);
+const mailOptions = {
+	from: '',
+	to: email,
+	subject: 'Ticket Booking Infomation',
+	html: compiledTemplate.render({ email }),
+};
+
+transporter.sendMail(mailOptions, err => {
+	if (err) return console.log(err);
+	console.log('Success');
+});
