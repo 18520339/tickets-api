48095095da541150b7edf78d326d8ff999f8b0ed
unknown <18520339@gm.uit.edu.vn>
2020-10-05 21:24:23 +0700

    edit user api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -14,12 +14,12 @@ const { authenticate, authorize } = require('../middleware/auth');
 const { uploadImage } = require('../middleware/image');
 const router = express.Router();
 
-router.post('/users', validateCreateUser, createUser);
-router.post('/users/login', login);
-router.patch('/users/update-password', updatePassword);
-router.get('/users/me', authenticate, authorize(['Admin', 'Member']), getMe);
+router.post('/user', validateCreateUser, createUser);
+router.post('/user/login', login);
+router.patch('/user/update-password', updatePassword);
+router.get('/user/me', authenticate, authorize(['Admin', 'Member']), getMe);
 router.post(
-	'/users/upload-avatar',
+	'/user/upload-avatar',
 	authenticate,
 	authorize(['Member']),
 	uploadImage('avatar'),
